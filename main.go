9105c0ff5658ef7d07ce71607a4d73fd8c34d310
ashfaqjani916 <ashfaqjani916@gmail.com>
2025-03-10 02:14:32 +0530

    [fix] removed loading of unnecessary files

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"log"
 
 	"web-scraper/cmd"
-	// "web-scraper/modules"
 
-	"github.com/joho/godotenv"
 )
 
 
 func main() {
-	err := godotenv.Load()
-  if err != nil { 
-    log.Fatal("Error loading .env file")
-  }
-	// modules.GetHackathonData()
-	cmd.Execute()
+cmd.Execute()
 }
 
